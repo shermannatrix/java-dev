7db00d6a1a56f44439a08bc21d1f6286c323be06
Sherman Chen <uxdailygeek@gmail.com>
2020-04-05 18:08:54 +0800

    Added code examples for Chapter03

---
+++ Java_Workshop/Chapter03/AnonymousClasses/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"AnonymousClasses","l":"AnonymousClasses()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Container","l":"Container()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"AnonymousClasses","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Container","l":"print()"}]
